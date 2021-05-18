99d842e0f441a287bfd029625a776cc46ce49d2c
Nicola <nicola.tamburini@fjstudio.com>
2021-05-18 11:46:06 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,48 @@
 <template>
+  <div>
+    <nav class="flex items-center justify-between flex-wrap bg-black p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <img class="h-10 w-10" alt="Vue logo" src="./assets/logo-pizzeria.jpg">
+      </div>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+          </svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+            Scegli quello che vuoi
+          </a>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+            Scegli l'orario
+          </a>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white">
+            Scegli tipo di consegna
+          </a>
+        </div>
+        <div>
+          <ShoppingCartIcon class="h-5 w-5 text-gray-300 hover:text-white" />
+        </div>
+      </div>
+    </nav>
+  </div>
+
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
+<script>
+  import {ShoppingCartIcon} from '@heroicons/vue/solid'
+  export default {
+    components: { ShoppingCartIcon }
+  }
+</script>
 
 <style>
 #app {
